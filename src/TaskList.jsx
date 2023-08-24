22a379021dce0c316a60c6d885622836fa4ff343
Christopher <chrismartinez.2054@gmail.com>
2023-08-23 22:53:50 -0600

    tasks

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
 
 function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+  if (tasks.length === 0) {
+    return <h1>No hay tareas</h1>;
+  }
+
   return (
-    <div>TaskList</div>
-  )
+    <div>
+      {tasks.map((task) => (
+        <div key= {task.id}>
+            <h1>{task.title}</h1>
+            <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default TaskList
+export default TaskList;
