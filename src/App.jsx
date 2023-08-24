a1b53b47534084c3efacdc9d435735e28df4d899
Christopher <chrismartinez.2054@gmail.com>
2023-08-23 23:37:23 -0600

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import TaskList from "./TaskList";
 import TaskForm from "./TaskForm";
+import { tasks as data } from "./tasks";
+import {useEffect, useState} from 'react'
 function App() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+
+
   return (
     <>
       <TaskForm />
-      <TaskList />
+      <TaskList tasks ={tasks}/>
     </>
   );
 }
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,12 +1,6 @@
-import React from "react";
-import { tasks as data } from "./tasks";
-import { useState, useEffect } from "react";
 
-function TaskList() {
-  const [tasks, setTasks] = useState([]);
-  useEffect(() => {
-    setTasks(data);
-  }, []);
+function TaskList({tasks}) {
+  
   if (tasks.length === 0) {
     return <h1>No hay tareas</h1>;
   }
