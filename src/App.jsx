f937f1568622a2cfdc1f1e5e881fc1bd97002ab8
daniel <danielcatlover8856@gmail.com>
2023-08-24 19:15:59 -0600

    completed js

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
-import { tasks as data } from "./data/tasks";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  useEffect(() => {
-    setTasks(data);
-  }, []);
-
-  function createTask(task) {
-    setTasks([
-      ...tasks,
-      {
-        title: task.title,
-        id: tasks.length,
-        description: task.description,
-      },
-    ]);
-  }
-
-  function deleteTask(taskId) {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  }
   return (
     <>
-      <TaskForm createTask={createTask} />
-      <TaskList tasks={tasks} deleteTask={deleteTask} />
+      <TaskForm />
+      <TaskList />
     </>
   );
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,10 @@
 import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContex } from "../context/TaskContex";
 
-function TaskList({ tasks, deleteTask }) {
+function TaskList() {
+  const { tasks } = useContext(TaskContex);
+
   if (tasks.length === 0) {
     return <h1>No hay tareas</h1>;
   }
@@ -8,7 +12,7 @@ function TaskList({ tasks, deleteTask }) {
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} deleteTask={deleteTask} />
+        <TaskCard key={task.id} task={task} />
       ))}
     </div>
   );
