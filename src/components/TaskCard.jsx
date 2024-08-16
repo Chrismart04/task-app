431339e470ee541bbe651738dc84a77ee497682e
Christopher <chrismartinez.2054@gmail.com>
2024-08-15 22:40:41 -0600

    added input validation to TaskForm

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,17 +1,22 @@
-import {useContext} from 'react'
-import {TaskContex} from '../context/TaskContex'
+import { useContext } from "react";
+import { TaskContex } from "../context/TaskContex";
 
-
-
-function TaskCard({ task}) {
-const {deleteTask}= useContext(TaskContex)
-
+function TaskCard({ task: { id, title, description } }) {
+  const { deleteTask } = useContext(TaskContex);
+  const handleDelete = () => {
+    deleteTask(id);
+  };
 
   return (
-    <div className='bg-gray-800 text-white p-4 rounded-lg'>
-      <h1 className='text-xl font-bold capitalize'>{task.title}</h1>
-      <p className='text-gray-500 text-sm'>{task.description}</p>
-      <button className='bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400' onClick={() => deleteTask(task.id)}>eliminar tarea</button>
+    <div className="bg-gray-800 text-white p-4 rounded-lg">
+      <h1 className="text-xl font-bold capitalize">{title}</h1>
+      <p className="text-gray-500 text-sm">{description}</p>
+      <button
+        className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400"
+        onClick={handleDelete}
+      >
+        eliminar tarea
+      </button>
     </div>
   );
 }
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,17 @@
-import { useState, useContext} from "react";
-import {TaskContex} from '../context/TaskContex'
+import { useState, useContext } from "react";
+import { TaskContex } from "../context/TaskContex";
 
 function TaskForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
- const {createTask}= useContext(TaskContex)
+  const { createTask } = useContext(TaskContex);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (title.trim() === "" || description.trim == "") {
+      alert("El titulo y la descripción no pueden estar vacios ");
+      return;
+    }
     createTask({
       title,
       description,
@@ -19,6 +23,7 @@ function TaskForm() {
     <div className="max-w-md mx-auto">
       <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
         <h1 className="text-2xl text-white font-bold mb-4 ">Crea tu tarea</h1>
+
         <input
           placeholder="escribe tu tarea"
           onChange={(e) => setTitle(e.target.value)}
@@ -26,14 +31,16 @@ function TaskForm() {
           className="bg-slate-300 p-3 w-full mb-2 rounded-lg"
           autoFocus
         />
-        
+
         <textarea
           placeholder="Escribe la descripcion de la tarea"
           onChange={(e) => setDescription(e.target.value)}
           value={description}
           className="bg-slate-300 p-3 w-full mb-2 rounded-lg"
         ></textarea>
-        <button className="bg-indigo-400 px-3 py-2 rounded-lg  text-white">Guardar</button>
+        <button className="bg-indigo-400 px-3 py-2 rounded-lg  text-white">
+          Guardar
+        </button>
       </form>
     </div>
   );
